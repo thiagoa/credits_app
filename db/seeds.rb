9f0149e4557e282297b0d34e3c5c64de12f30660
Thiago A. Silva <thiagoaraujos@gmail.com>
2016-07-25 00:35:59 -0300

    Seed for 1 million credits

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Credit.delete_all
+User.delete_all
+
+User.import(30_000.times.map { User.random })
+credits_generator = RandomCreditsGenerator.new(user_ids: User.pluck(:id))
+
+TOTAL_CREDITS = 1_000_000
+BATCH_SIZE = 20_000
+
+cols = ''
+credits = ''
+
+import_credits = -> {
+  sql = "INSERT INTO credits(#{cols}, updated_at) VALUES #{credits}"
+  Credit.connection.execute(sql) unless credits.empty?
+
+  credits = ''
+}
+
+TOTAL_CREDITS.times do |i|
+  is_first = (i % BATCH_SIZE).zero?
+  import_credits.() if is_first
+
+  credit = credits_generator.call(with_expiration: i > 30_000)
+  cols = credit.keys.join(', ') if cols.empty?
+  values = credit.values.map { |v| Credit.connection.quote(v) }.join(', ')
+  values += ", #{Credit.connection.quote(credit[:created_at])}"
+
+  credits += %{#{is_first ? '' : ', '}(#{values})}
+end
+
+import_credits.()
+
+User.where.not(id: Credit.select(:user_id).uniq).delete_all
